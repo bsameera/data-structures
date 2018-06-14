880042a26544eef2391aa7952885bd118eabf852
bsameera <bsameera79@gmail.com>
2018-06-13 18:06:10 -0700

    completed prototypalinheritance for stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(queueMethods);
+  storage.first = 0;
+  storage.last = 0;
+
+  return storage;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this[this.last] = value;
+  this.last++;
+};
+
+queueMethods.dequeue = function() {
+  var value = this[this.first];
+  this.first++;
+  return value;
+};
+
+queueMethods.size = function() {
+  var size = this.last - this.first;
+  return size > 0 ? size : 0;
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(stackMethods);
+  storage.first = 0;
+  storage.last = 0;
+
+  return storage;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this[this.last] = value;
+  this.last++;
+};
 
+stackMethods.pop = function() {
+  var value = this[this.last - 1];
+  this.last--;
+  return value;
+};
+
+stackMethods.size = function() {
+  var size = this.last - this.first;
+  return size > 0 ? size : 0;
+};
